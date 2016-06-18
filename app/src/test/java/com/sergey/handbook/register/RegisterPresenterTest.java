cd87c5d67fe43810fb25a76b2b4282c26dafb380
Sergey <kuchinsergey5002@gmail.com>
2016-06-19 01:23:52 +0500

    попытка сделать тесты

--- app/src/test/java/com/sergey/handbook/register/RegisterPresenterTest.java
+++ app/src/test/java/com/sergey/handbook/register/RegisterPresenterTest.java
@@ -1,5 +1,7 @@
 package com.sergey.handbook.register;
 
+import android.content.Context;
+
 import com.sergey.handbook.R;
 
 import org.junit.Before;
@@ -20,11 +22,15 @@ public class RegisterPresenterTest {
 
     @Mock
     private RegisterView view;
-    @Mock
+
     private RegisterService service;
+
+    @Mock
+    private Context context;
 
     @Before
     public void setUp() throws Exception {
+        service = new RegisterService(context);
         presenter = new RegisterPresenter(view, service);
     }
 
@@ -39,6 +45,7 @@ public class RegisterPresenterTest {
     @Test
     public void testShoudlShowErrorMessageWhenPhoneIsIncorrect() throws Exception {
         when(view.getPhoneNumber()).thenReturn("88005553535");
+        when(service.isPhoneNumberCorrect("88005553535")).thenReturn(false);
         presenter.onRegisterClicked();
 
         verify(view).showPhoneNumberError(R.string.phonenumber_incorrect_error);
@@ -47,6 +54,7 @@ public class RegisterPresenterTest {
     @Test
     public void testShouldShowErrorMessageIfPhoneIsNotInDB() throws Exception {
         when(view.getPhoneNumber()).thenReturn("89505439888");
+        //when(service.isPhoneNumberCorrect("89505439888")).thenReturn(true);
         presenter.onRegisterClicked();
 
         verify(view).showPhoneNumberError(R.string.phonenumber_notin_DB);
